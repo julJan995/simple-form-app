1a23560834ff9e324f199a370afd82e1a7cca332
juliajanus <julia.janus.dev@gmail.com>
2025-09-03 17:15:20 +0200

    feat: basic implementation of the contact form

---
+++ src/app/components/submit-form/submit-form.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-submit-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './submit-form.component.html',
+  styleUrl: './submit-form.component.scss'
+})
+export class SubmitFormComponent {
+
+  contactForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    queryType: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+    consent: new FormControl(false, Validators.requiredTrue)
+  })
+}
+
+
